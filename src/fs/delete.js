0ce80bcff72521afde1deb5392f5c64ee8fa1f89
Larisa Novozhilova <kara4516@gmail.com>
2024-10-03 15:12:11 +0300

    feat: solved delete task

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,22 @@
+import { unlink, access, constants } from "fs";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const remove = async () => {
-    // Write your code here 
+  const deletedFilePath = join(__dirname, "files", "fileToRemove.txt");
+
+  access(deletedFilePath, constants.F_OK, (err) => {
+    if (err) {
+      throw new Error("FS operation failed");
+    }
+
+    unlink(deletedFilePath, (err) => {
+      if (err) throw new Error("FS operation failed");
+    });
+  });
 };
 
 await remove();
