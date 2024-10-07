5d0b1cca78155da9bc8668f6ae1a65cc44fa7b20
Larisa Novozhilova <kara4516@gmail.com>
2024-10-07 14:39:56 +0300

    feat: solved read task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,23 @@
+import { readFile, access, constants } from "fs";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+  const filePath = join(__dirname, "files", "fileToRead.txt");
+
+  access(filePath, constants.F_OK, (err) => {
+    if (err) {
+      throw new Error("FS operation failed");
+    }
+
+    readFile(filePath, "utf8", (err, data) => {
+      if (err) throw err;
+      console.log(data);
+    });
+  });
 };
 
 await read();
